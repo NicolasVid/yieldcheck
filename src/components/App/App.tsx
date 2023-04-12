7778416978e7b5fbc53be8d5dd84890a7654340e
NicolasVid <65497850+NicolasVid@users.noreply.github.com>
2023-04-12 20:46:01 +0200

    chore: add slt mention

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,26 @@
+import { Box, Link, Typography } from '@mui/material'
 import MainTab from '../MainTab'
 
 const App = () => {
   const tabList: string[] = ['Rendement', 'Cash Flow']
-  return <MainTab tabList={tabList} />
+  return (
+    <Box>
+      <Typography
+        textAlign="center"
+        sx={{ backgroundColor: 'grey.100', padding: '0.2rem' }}
+      >
+        Suivez vos loyers simplement et gratuitement avec notre application
+        <br />
+        <Link
+          href="https://www.suistesloyers.fr"
+          sx={{ textDecoration: 'none', fontWeight: 'bold' }}
+        >
+          Suis Tes Loyers
+        </Link>
+      </Typography>
+      <MainTab tabList={tabList} />
+    </Box>
+  )
 }
 
 export default App
